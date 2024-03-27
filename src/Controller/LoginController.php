f8ae7b218aa8ba777a6cda244ef2a6263f72781f
Moncyyy <moncef-benkhalfallah@outlook.com>
2024-03-27 15:24:47 +0100

    POST user request

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -37,5 +38,28 @@ class LoginController extends AbstractController
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/LoginController.php',
         ]);
+    }
+    #[Route('/user', name: 'app_create_user', methods: ['POST'])]
+    public function createUser(Request $request): JsonResponse
+    {
+        $requestData = json_decode($request->getContent(), true);
+
+        $user = new User();
+        $user->setIdUser($requestData['idUser'])
+            ->setName($requestData['name'])
+            ->setEmail($requestData['email'])
+            ->setEncrypte($requestData['encrypte'])
+            ->setTel($requestData['tel'])
+            ->setCreateAt(new \DateTimeImmutable())
+            ->setUpdateAt(new \DateTime());
+
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+
+        return $this->json([
+            'user' => $user->userSerializer(),
+            'message' => 'User created successfully!',
+            'path' => 'src/Controller/UserController.php',
+        ], Response::HTTP_CREATED);
     }
 }
